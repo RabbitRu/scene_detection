d091abb24684d63204aaa627de9be61620209a1a
Egor Domnin <zombie9@inbox.ru>
2020-03-01 14:07:37 +0300

    Добавил метрики coverage\overflow

--- checkResults.py
+++ checkResults.py
@@ -1,11 +1,12 @@
 from numpy import loadtxt
 
+#Во всех метриках А оригинал, B сгенерирован
 
-
-
+# здесь можно добавить замер времени
 def planLength(division, index):
 	return division[index] - division[index - 1]
 
+# здесь можно добавить замер времени
 def planIntersection(divA, indexA, divB, indexB):
 	return max(min(divA[indexA], divB[indexB]) - max(divA[indexA - 1], divB[indexB - 1]), 0)
 
@@ -32,16 +33,64 @@ def metricPurity(divA, divB):
 		print("Такого быть не должно, количество планов должно совпадать")
 	totalLength = divB[-1]
 	leftPart = puritySubfunc(divA, divB, totalLength)
-	#rightPart = puritySubfunc(divB, divA, totalLength)
-	return leftPart# * rightPart
+	rightPart = puritySubfunc(divB, divA, totalLength)
+	return leftPart * rightPart
 
 
+
+
+
+def findOverlappings(division, shotStart, shotEnd):
+	leftOverlap = 0
+	rightOverlap = 0 
+	for i in range(1, len(division)):
+		if(shotStart < division[i - 1] and division[i - 1] < shotEnd):
+			leftOverlap = division[i - 1] - shotStart
+		if(shotStart < division[i] and division[i] < shotEnd):
+			rightOverlap = division[i] - shotStart
+	totalOverlap = leftOverlap + rightOverlap # здесь можно добавить замер времени
+	return totalOverlap
+
+def metricOverflow(divA, divB):
+	summator = 0
+	for i in range(1, len(divB)):
+		summator = summator + findOverlappings(divA, divB[i - 1], divB[i])
+	return summator / divB[-1]
+
+
+def findLagrestOverlappingScene(division, shotStart, shotEnd):
+	maxIntersec = 0
+	for i in range(1, len(division)):
+		intersection = 0
+		if not (division[i] < shotStart or division[i - 1] > shotEnd):
+			if(division[i] < shotEnd):
+				intersection = shotEnd - division[i]# здесь можно добавить замер времени
+			else:
+				intersection = min(shotEnd - shotStart, shotEnd - division[i - 1])# здесь можно добавить замер времени
+		if(intersection > maxIntersec):
+			maxIntersec = intersection
+	if(maxIntersec == 0):
+		print("Для каждой сцены должно быть пересечение хоть в один план")
+	return maxIntersec 
+
+def metricCoverage(divA, divB):
+	summator = 0
+	for i in range(1,len(divA)):
+		summator = summator + findLagrestOverlappingScene(divB, divA[i - 1], divA[i]) / (divA[i] - divA[i - 1])
+	return summator / len(divA)
 
 
 
 division = loadtxt("division.txt", int )
 purity = metricPurity(division, division)
 print("Purity " + str(purity))
+
+coverage = metricCoverage(division, division)
+print("Coverage " + str(coverage))
+
+
+overflow = metricOverflow(division, division)
+print("Overflow " + str(overflow))
 
 
 
